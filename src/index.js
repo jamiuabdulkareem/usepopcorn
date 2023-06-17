5576a673446fb2dcafd45c0af852f670bef07d46
jamiuabdulkareem <jamiuabdulkareem08@gmail.com>
2023-06-17 20:34:02 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StartRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating maxRating={8} onSetRating={setMovieRating} color="red" />
+      <p>This movie was rate {movieRating}</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={32} color="#0ee" maxRating={10} defaultRating={3} />
+    <Test />
+    {/* <StarRating /> */}
     {/* <App /> */}
   </React.StrictMode>
 );
